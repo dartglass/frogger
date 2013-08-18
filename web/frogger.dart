c08d4c70df83eee954612f96275b202741a0b99a
Adam Singer <financeCoding@gmail.com>
2013-08-17 17:05:18 -0700

    Adding a game loop

--- web/frogger.dart
+++ web/frogger.dart
@@ -1,5 +1,19 @@
 import 'dart:html';
+import 'package:game_loop/game_loop_html.dart';
 
 void main() {
+  CanvasElement canvasElement = query("#canvas");
 
+  // Construct a game loop.
+  GameLoopHtml gameLoop = new GameLoopHtml(canvasElement);
+  gameLoop.onUpdate = ((gameLoop) {
+    // Update game logic here.
+    print('${gameLoop.frame}: ${gameLoop.gameTime} [dt = ${gameLoop.dt}].');
+  });
+  gameLoop.onRender = ((gameLoop) {
+    // Draw game into canvasElement using WebGL or CanvasRenderingContext here.
+    // The interpolation factor can be used to draw correct inter-frame
+    print('Interpolation factor: ${gameLoop.renderInterpolationFactor}');
+  });
+  gameLoop.start();
 }
