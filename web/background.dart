eee045a38207f888c35604b286960efddbfd5305
Adam Singer <financeCoding@gmail.com>
2013-08-17 21:37:47 -0700

    Placing in background image

--- web/background.dart
+++ web/background.dart
@@ -2,8 +2,12 @@ part of main;
 
 class Background {
   Game game;
-  Background(this.game);
+  ImageElement backgroundImage;
+  Background(this.game) {
+    backgroundImage = new Element.tag('img');
+    backgroundImage.src = "images/game-field.jpg";
+  }
   draw() {
-
+    game.ctx.drawImage(backgroundImage, 0, 0);
   }
 }
