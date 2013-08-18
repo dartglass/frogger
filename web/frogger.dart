c41c81f4cfe61be79f6ebd04fdd9afcdb387d3fb
Adam Singer <financeCoding@gmail.com>
2013-08-17 18:09:41 -0700

    Adding a debug draw grid

--- web/frogger.dart
+++ web/frogger.dart
@@ -16,12 +16,12 @@ void main() {
   GameLoopHtml gameLoop = new GameLoopHtml(canvasElement);
   gameLoop.onUpdate = ((gameLoop) {
     // Update game logic here.
-    print('${gameLoop.frame}: ${gameLoop.gameTime} [dt = ${gameLoop.dt}].');
+    //print('${gameLoop.frame}: ${gameLoop.gameTime} [dt = ${gameLoop.dt}].');
   });
   gameLoop.onRender = ((gameLoop) {
     // Draw game into canvasElement using WebGL or CanvasRenderingContext here.
     // The interpolation factor can be used to draw correct inter-frame
-    print('Interpolation factor: ${gameLoop.renderInterpolationFactor}');
+    //print('Interpolation factor: ${gameLoop.renderInterpolationFactor}');
     game.draw();
   });
 
@@ -38,12 +38,16 @@ class Game {
   Background background;
   Frogger frogger;
   List<Car> cars;
+  TestGrid testGrid;
 
   Game(this.canvas) {
     ctx = canvas.getContext('2d');
     height = canvas.height;
     width = canvas.width;
+    print("canvas.height = $height");
+    print("cavnas.width = $width");
 
+    testGrid = new TestGrid(this, width, height);
     background = new Background(this);
     frogger = new Frogger(this);
     cars = new List<Car>();
@@ -56,7 +60,7 @@ class Game {
 
   draw() {
     clear(ctx, width, height);
-
+    testGrid.draw();
     background.draw();
     frogger.draw();
     cars.forEach((Car car) => car.draw());
@@ -84,5 +88,46 @@ class Car {
   Car(this.game);
   draw() {
 
+  }
+}
+
+class TestGrid {
+  Game game;
+  CanvasRenderingContext2D ctx;
+  int lanes = 9;
+  int rows = 5;
+  int height;
+  int width;
+  int widthSpacing;
+  int heightSpacing;
+
+  TestGrid(this.game, this.width, this.height) {
+    widthSpacing = (width/lanes).toInt();
+    heightSpacing = (height/rows).toInt();
+    print("heightSpacing = $heightSpacing");
+    print("widthSpacing = $widthSpacing");
+    ctx = game.ctx;
+  }
+
+  draw() {
+    for (int row = 0; row < rows; row++) {
+      drawTestLine(0, row * heightSpacing, width, 0);
+    }
+
+    for (int lane = 0; lane < lanes; lane++) {
+      drawTestLine(lane * widthSpacing, 0, 0, height);
+    }
+  }
+
+  void drawTestLine(x, y, changeX, changeY) {
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+    ctx.lineTo(x + changeX, y + changeY);
+    ctx.stroke();
+    ctx.closePath();
   }
 }
