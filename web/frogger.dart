05ab55d8b4fe592ec316574050c429fa31089733
Adam Singer <financeCoding@gmail.com>
2013-08-17 17:31:23 -0700

    Adding background class

--- web/frogger.dart
+++ web/frogger.dart
@@ -1,8 +1,16 @@
 import 'dart:html';
 import 'package:game_loop/game_loop_html.dart';
 
+CanvasElement canvasElement;
+
+clear(CanvasRenderingContext2D ctx, int width, int height) =>
+  ctx.clearRect(0, 0, width, height);
+
+
 void main() {
-  CanvasElement canvasElement = query("#canvas");
+  canvasElement = query("#canvas");
+
+  Game game = new Game(canvasElement);
 
   // Construct a game loop.
   GameLoopHtml gameLoop = new GameLoopHtml(canvasElement);
@@ -14,6 +22,44 @@ void main() {
     // Draw game into canvasElement using WebGL or CanvasRenderingContext here.
     // The interpolation factor can be used to draw correct inter-frame
     print('Interpolation factor: ${gameLoop.renderInterpolationFactor}');
+    game.draw();
   });
+
+  game.init();
   gameLoop.start();
+}
+
+class Game {
+  CanvasElement canvas;
+  int height;
+  int width;
+  CanvasRenderingContext2D ctx;
+
+  Background background;
+
+  Game(this.canvas) {
+    ctx = canvas.getContext('2d');
+    height = canvas.height;
+    width = canvas.width;
+
+    background = new Background(this);
+  }
+
+  init() {
+
+  }
+
+  draw() {
+    clear(ctx, width, height);
+
+    background.draw();
+  }
+}
+
+class Background {
+  Game game;
+  Background(this.game);
+  draw() {
+
+  }
 }
